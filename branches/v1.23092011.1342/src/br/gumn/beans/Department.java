daec2f3e648b20b047af264ca66a389bc02a81cc
gertmuller88@gmail.com <gertmuller88@gmail.com@6aeae1c6-f092-6bce-e37f-9f28d9b32b20>
2011-09-23 16:43:16 +0000

    Development line.

---
+++ branches/v1.23092011.1342/src/br/gumn/beans/Department.java
@@ -0,0 +1,51 @@
+package br.gumn.beans;
+
+import java.util.List;
+
+public class Department {
+	private int id;
+	private String acronym;
+	private String name;
+	private University university;
+	private List<Researcher> members;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAcronym() {
+		return acronym;
+	}
+
+	public void setAcronym(String acronym) {
+		this.acronym = acronym;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public University getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(University university) {
+		this.university = university;
+	}
+
+	public List<Researcher> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<Researcher> members) {
+		this.members = members;
+	}
+}
